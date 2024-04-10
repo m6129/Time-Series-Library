a6db6340dc605a4746e1d9d525db2915aa6d6489
Anton <104712265+m6129@users.noreply.github.com>
2024-04-10 07:45:52 +0300

    add Loader
    'exchange_rate': exchange_rateLoader,
        'weather': weatherLoader,
        'illness': illnessLoader
    }

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,5 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, exchange_rateLoader, weatherLoader, illnessLoader
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
@@ -15,7 +15,10 @@ data_dict = {
     'SMAP': SMAPSegLoader,
     'SMD': SMDSegLoader,
     'SWAT': SWATSegLoader,
-    'UEA': UEAloader
+    'UEA': UEAloader,
+    'exchange_rate': exchange_rateLoader,
+    'weather': weatherLoader,
+    'illness': illnessLoader
 }
 
 
